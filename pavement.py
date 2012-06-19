1858979fc002f2f3eb02d4eb3422474141f19c48
Kristoffer Sall Hansen <ksh@attention8.dk>
2012-06-19 10:29:40 +0200

    Second draft

--- pavement.py
+++ pavement.py
@@ -6,6 +6,9 @@ def generate():
     """Generate github pages"""
     sh("rm -rf build deploy dist pyGitDeplooy.egg-info")
     sh("rm -rf .gitattributes .gitignore")
-    sh("rm paver-minilib.zip deploy.cfg *.md setup.py")
-    sh("mv docs/* .")
+    sh('rm paver-minilib.zip deploy.cfg *.md setup.py')
+    sh("cd docs")
+    sh('mv * ..')
+    sh("cd ..")
     sh("mv deploy.html index.html")
+    sh('rm -rf docs')
